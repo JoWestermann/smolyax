Overhauled generation of test polynomials. Correct scaling allows for more comprehensive tests

tests/setup.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numpy.polynomial.legendre import legval
+from numpy.polynomial.hermite import hermval
 
-from jax_smolyak.points import *
+from jax_smolyak import points, indices
 
 
 def generate_pointsets(*, n, dmin, dmax):
@@ -12,11 +14,48 @@ def generate_pointsets(*, n, dmin, dmax):
 
         m = np.random.randn(d)
         a = np.random.rand(d)
-        sets.append(GaussHermiteMulti(m, a))
+        sets.append(points.GaussHermiteMulti(m, a))
 
         domain = np.zeros((d, 2))
         domain[:, 1] = np.sort(np.random.rand(d) * 10)
         domain[:, 0] = -domain[:, 1]
-        sets.append(LejaMulti(domains=domain))
+        sets.append(points.LejaMulti(domains=domain))
 
     return sets
+
+
+def evaluate_univariate_polynomial(g, nu, x) :
+    x = g.scale_back(x)
+    if isinstance(g, points.LejaMulti) :
+        return legval(x, [0] * nu + [1])
+    else :
+        return hermval(x, [0] * nu + [1])
+
+
+def evaluate_multivariate_polynomial(g, nu, x) :
+    if x.ndim <= 1 :
+        return np.prod([evaluate_univariate_polynomial(gi, nui, xi) for gi, nui, xi in zip(g, nu, x)])
+    elif x.ndim == 2 :
+        return np.prod(np.array([evaluate_univariate_polynomial(gi, nui, xi) for gi, nui, xi in zip(g, nu, x.T)]), axis=0).T
+    else :
+        raise
+
+
+def generate_test_function_tensorproduct(*, g, nu) :
+    if np.isscalar(nu) :
+        return lambda x: evaluate_univariate_polynomial(g, nu, x)
+    return lambda x: evaluate_multivariate_polynomial(g, nu, x)
+
+
+def generate_test_function_smolyak(*, g, k, l, d_out) :
+    if np.isscalar(l) :
+        l = [l] * d_out
+    assert len(l) == d_out
+
+    selected_idxs = []
+    for li in l :
+        idxs = indices.indexset_sparse(lambda j: k[j], li, cutoff=len(k))
+        j = np.random.randint(len(idxs))
+        selected_idxs.append(indices.sparse_index_to_dense(idxs[j], cutoff=len(k)))
+    print('\t Test polynomials with degrees', selected_idxs)
+    return lambda x: np.array([evaluate_multivariate_polynomial(g, nu, x) for nu in selected_idxs]).T

tests/test_smolyak.py
@@ -1,67 +1,53 @@
-import numpy as np
 import setup
-import test_tensorproduct
 
 from jax_smolyak.smolyak import *
 
+import numpy as np
+import warnings
 
-def generate_test_function_univariate(ip, n=1):
-    coeffs = np.random.rand(n) * 2 - 1
-    coeffs /= len(coeffs)
-    idxs = np.random.randint(low=0, high=len(ip.operators), size=n)
-    fs = [test_tensorproduct.generate_test_function(ip.operators[i]) for i in idxs]
-
-    def test_f(x, coeffs, fs):
-        res = 0
-        for c, fi in zip(coeffs, fs):
-            res += c * fi(x)
-        return res
-
-    return lambda x: test_f(x, coeffs, fs)
+np.seterr(all='raise')  # Treat floating-point warnings as errors
+warnings.simplefilter("error")  # Treat all Python warnings as errors
 
 
-def generate_test_function_multivariate(ip):
-    fs = [generate_test_function_univariate(c) for c in ip.components]
-    return lambda x: np.array([fi(x) for fi in fs])
+def test_smolyak_scalar():
+    print("\nTesting scalar-valued Smolyak operator (numpy) ...")
 
-
-def test_smolyak_univariate():
-
-    for g in setup.generate_pointsets(n=10, dmin=2, dmax=2):
+    for g in setup.generate_pointsets(n=10, dmin=1, dmax=4):
 
         k = sorted(np.random.randint(low=1, high=10, size=g.d))
         k /= k[0]
-        print(f"Testing with d = {g.d}, k = {k}")
-        # g.print()
+        l = np.random.randint(low=1, high=4)
+        print(f"... with k = {k}, l = {l},", g)
 
-        ip = SmolyakBarycentricInterpolator(g, k, 2)
-        f = generate_test_function_univariate(ip)
+        ip = SmolyakBarycentricInterpolator(g, k, l)
+        ff = setup.generate_test_function_smolyak(g=g, k=k, l=l, d_out=1)
+        f = lambda x : np.squeeze(ff(x))
         ip.set_F(f)
 
         for n in range(5):
-            x = g.get_random()
-            print(f"\t\t ip(x) = {ip(x)}, f(x) = {f(x)}")
-            assert np.isclose(
+            x = g.get_random(n=np.random.randint(low=0, high=5))
+            assert np.allclose(
                 ip(x), f(x)
             ), f"Assertion failed with\n x = {x}\n f(x) = {f(x)}\n ip(x) = {ip(x)}"
 
 
-def test_smolyak_multivariate():
+def test_smolyak_vector():
+    print("\nTesting vector-valued Smolyak operator (numpy) ...")
 
-    for g in setup.generate_pointsets(n=10, dmin=2, dmax=5):
+    for g in setup.generate_pointsets(n=10, dmin=1, dmax=4):
 
         k = sorted(np.random.randint(low=1, high=10, size=g.d))
         k /= k[0]
-        d2 = np.random.randint(low=1, high=5)
-        k2 = sorted(np.random.randint(low=1, high=4, size=d2), reverse=True)
-        print(f"Testing with d2 = {d2}, k2 = {k2}")
+        d_out = np.random.randint(low=1, high=5)
+        l = sorted(np.random.randint(low=1, high=4, size=d_out), reverse=True)
+        print(f"... with k = {k}, l = {np.array(l).tolist()},", g)
 
-        ip = MultivariateSmolyakBarycentricInterpolator(g=g, k=k, l=k2)
-        f = generate_test_function_multivariate(ip)
+        ip = MultivariateSmolyakBarycentricInterpolator(g=g, k=k, l=l)
+        f = setup.generate_test_function_smolyak(g=g, k=k, l=l, d_out=d_out)
         ip.set_F(f=f)
 
         for n in range(5):
-            x = g.get_random()
+            x = g.get_random(n=np.random.randint(low=0, high=5))
             print(f"\t\t ip(x) = {ip(x)}, f(x) = {f(x)}")
             assert np.allclose(
                 ip(x), f(x)

tests/test_smolyak_jax.py
@@ -1,30 +1,26 @@
 import numpy as np
 import setup
-import test_smolyak
 
-from jax_smolyak.smolyak import MultivariateSmolyakBarycentricInterpolator as SmolN
-from jax_smolyak.smolyak_jax import MultivariateSmolyakBarycentricInterpolator as SmolJ
+from jax_smolyak.smolyak_jax import MultivariateSmolyakBarycentricInterpolator as Smol
 
 
 def test_smolyak_jax():
+    print("\nTesting vector-valued Smolyak operator (jax) ...")
 
-    for g in setup.generate_pointsets(n=10, dmin=2, dmax=2):
+    for g in setup.generate_pointsets(n=10, dmin=1, dmax=4):
 
         k = sorted(np.random.randint(low=1, high=10, size=g.d))
         k /= k[0]
-        d2 = np.random.randint(low=1, high=4)
-        k2 = np.random.randint(low=1, high=4)
-        print(f"Testing with d2 = {d2}, k2 = {k2}")
+        d_out = np.random.randint(low=1, high=4)
+        l = np.random.randint(low=1, high=4)
+        print(f"... with k = {k}, l = {l}, d_out = {d_out},", g)
 
-        ipN = SmolN(g=g, k=k, l=[k2] * d2)
-        f = test_smolyak.generate_test_function_multivariate(ipN)
+        f = setup.generate_test_function_smolyak(g=g, k=k, l=l, d_out=d_out)
 
-        ipJ = SmolJ(g=g, k=k, l=k2, rank=d2, batchsize=1, f=f)
+        ip = Smol(g=g, k=k, l=l, rank=d_out, batchsize=1, f=f)
 
         for n in range(5):
-            x = g.get_random()
-            y = f(x)
-            y_ = ipJ(x)
+            x = g.get_random(n=np.random.randint(low=0, high=5))
             assert np.allclose(
-                y, y_
-            ), f"Assertion failed with\n x = {x}\n f(x) = {y}\n ip(x) = {y_} @ n = {n}"
+                f(x), ip(x)
+            ), f"Assertion failed with\n x = {x}\n f(x) = {f(x)}\n ip(x) = {ip(x)} @ n = {n}"