ddebac10ac772011378de110d913adb03235066e
Josephine Westermann <josephine.westermann@uni-heidelberg.de>
2025-03-20 10:36:42 +0100

    Split up nodes.py into files for abcs, leja and gausshermite

---
+++ src/jax_smolyak/nodes/base.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+from typing import Iterator, List, Self
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+
+class Generator(ABC):
+    """Abstract base class for interpolation points"""
+
+    def __init__(self, dim: int, is_nested: bool) -> None:
+        self._dim = dim
+        self._is_nested = is_nested
+
+    @property
+    def is_nested(self) -> bool:
+        return self._is_nested
+
+    @property
+    def dim(self) -> int:
+        return self._dim
+
+    @abstractmethod
+    def __call__(self, n: int) -> ArrayLike: ...
+
+    def __getitem__(self, i: int):
+        assert i == 0
+        return self
+
+    def __iter__(self) -> Iterator[Self]:
+        for index in range(self.dim):
+            yield self[index]
+
+    @abstractmethod
+    def scale(self, x: ArrayLike) -> ArrayLike: ...
+
+    @abstractmethod
+    def scale_back(self, x: ArrayLike) -> ArrayLike: ...
+
+    @abstractmethod
+    def get_random(self, n: int = 1) -> ArrayLike: ...
+
+    def get_zero(self) -> ArrayLike:
+        return self(0)[0]
+
+    @abstractmethod
+    def __repr__(self) -> str: ...
+
+
+class GeneratorMultiD(Generator):
+
+    def __init__(self, node_gens: List[Generator]):
+        assert all(g.is_nested == node_gens[0].is_nested for g in node_gens)
+        super().__init__(dim=len(node_gens), is_nested=node_gens[0].is_nested)
+        self.gens = node_gens
+
+    def __call__(self, n: int) -> ArrayLike:
+        raise
+
+    def __getitem__(self, i: int) -> Generator:
+        return self.gens[i]
+
+    def get_zero(self) -> ArrayLike:
+        return np.array([g(0)[0] for g in self.gens])
+
+    def get_random(self, n: int = 0) -> ArrayLike:
+        if n == 0:
+            return np.squeeze([g.get_random() for g in self.gens])
+        return np.array([g.get_random(n) for g in self.gens]).T
+
+    def scale(self, x: ArrayLike) -> ArrayLike:
+        assert x.shape[-1] == self.dim
+        if x.ndim == 1:
+            return np.array([g.scale(xi) for g, xi in zip(self.gens, x)])
+        else:
+            return np.array([g.scale(xi) for g, xi in zip(self.gens, x.T)]).T
+
+    def scale_back(self, x: ArrayLike) -> ArrayLike:
+        assert x.shape[-1] == self.dim
+        if x.ndim == 1:
+            return np.array([g.scale_back(xi) for g, xi in zip(self.gens, x)])
+        else:
+            return np.array([g.scale_back(xi) for g, xi in zip(self.gens, x.T)]).T
+
+    @abstractmethod
+    def __repr__(self) -> str: ...
---
+++ src/jax_smolyak/nodes/gausshermite.py
@@ -0,0 +1,52 @@
+import numpy as np
+from numpy.typing import ArrayLike
+
+from .base import Generator, GeneratorMultiD
+
+
+class GaussHermite1D(Generator):
+    """Gauss-Hermite grid points"""
+
+    def __init__(self, mean: float, scaling: float) -> None:
+        super().__init__(dim=1, is_nested=False)
+        self.mean = mean
+        self.scaling = scaling
+
+    def __repr__(self) -> str:
+        return f"Gauss-Hermite (mean = {self.mean}, scaling = {self.scaling})"
+
+    def __call__(self, n: int) -> ArrayLike:
+        nodes = np.polynomial.hermite.hermgauss(n + 1)[0]
+        return self.scale(nodes)
+
+    def scale(self, x: ArrayLike) -> ArrayLike:
+        return self.mean + self.scaling * x
+
+    def scale_back(self, x: ArrayLike) -> ArrayLike:
+        return (x - self.mean) / self.scaling
+
+    def get_random(self, n: int = 1) -> ArrayLike:
+        return self.scale(np.random.randn(n))
+
+
+class GaussHermite(GeneratorMultiD):
+
+    def __init__(self, mlist: ArrayLike, alist: ArrayLike):
+        GeneratorMultiD.__init__(
+            self, [GaussHermite1D(m, a) for m, a in zip(mlist, alist)]
+        )
+        self.mean = np.array(mlist)
+        self.scaling = np.array(alist)
+
+    def __repr__(self) -> str:
+        return (
+            f"Gauss Hermite (d = {self.dim}"
+            f", mean = {self.mean.tolist()}"
+            f", scaling = {self.scaling.tolist()})"
+        )
+
+    def scale(self, x: ArrayLike) -> ArrayLike:
+        return self.mean + self.scaling * x
+
+    def scale_back(self, x: ArrayLike) -> ArrayLike:
+        return (x - self.mean) / self.scaling
