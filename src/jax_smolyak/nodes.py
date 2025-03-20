a826d84bf76bc06c753ae6d1ae02f5c3d682de3f
Josephine Westermann <josephine.westermann@uni-heidelberg.de>
2025-03-20 10:25:01 +0100

    Let node classes overwrite scale and scale_back for more efficient implementation

--- src/jax_smolyak/nodes.py
+++ src/jax_smolyak/nodes.py
@@ -132,6 +132,12 @@ class GaussHermite(GeneratorMultiD):
             f", scaling = {self.scaling.tolist()})"
         )
 
+    def scale(self, x: ArrayLike) -> ArrayLike:
+        return self.mean + self.scaling * x
+
+    def scale_back(self, x: ArrayLike) -> ArrayLike:
+        return (x - self.mean) / self.scaling
+
 
 # =========== Leja nodes ===========
 
@@ -173,57 +179,40 @@ class Leja1D(Generator):
     ) -> ArrayLike:
         """
         Affine transformation from the interval d1 to the interval d2 applied to point x.
-        x : scalar or array or list of shape (n, d) or (d, )
-        d1, d2 : arrays or lists of shape (d, 2) or (2, )
+        x : scalar or array or list of shape (n, )
+        d1, d2 : arrays or lists of shape (2, )
         """
         if d1 is None:
             d1 = self._reference_domain
         if d2 is None:
             d2 = self.domain
 
-        # ensure d1, d2 have shape (d, 2)
-        d1, d2 = np.array(d1), np.array(d2)
-        assert d1.shape == d2.shape
-        d = 1 if len(d1.shape) == 1 else d1.shape[0]
-        if len(d1.shape) == 1:
-            d1 = d1.reshape((1, 2))
-            d2 = d2.reshape((1, 2))
-        for i in range(d):
-            assert d1[i, 0] < d1[i, 1]
-            assert d2[i, 0] < d2[i, 1]
+        # ensure d1, d2 have shape (2, )
+        d1, d2 = np.squeeze(d1), np.squeeze(d2)
+        assert (
+            d1.shape == d2.shape == (2,)
+        ), f"shapes {d1.shape} and {d2.shape} do not match (2, )"
+        assert d1[0] < d1[1]
+        assert d2[0] < d2[1]
 
-        # ensure x has shape (n, d)
-        x = np.array(x)
+        # ensure x has shape (n, )
         x_shape = x.shape
-        if x_shape == ():
-            x = np.array([[x]])
-        else:
-            x = np.array(x)
-            if len(x.shape) == 1:
-                if x.shape[0] == d:
-                    x = x.reshape((1, len(x)))
-                else:
-                    x = x.reshape((len(x), 1))
+        x = np.squeeze(x)
+        assert x.ndim <= 1
 
         # ensure x in d1
-        for i in range(d):
-            for j in range(x.shape[0]):
-                assert x[j, i] >= d1[i, 0] or np.isclose(
-                    x[j, i], d1[i, 0]
-                ), f"Assertion failed with\n x[{j},{i}] ({x[j, i]})\n d1[{i},0] ({d1[i, 0]})"
-                assert x[j, i] <= d1[i, 1] or np.isclose(
-                    x[j, i], d1[i, 1]
-                ), f"Assertion failed with\n x[{j},{i}] ({x[j, i]})\n d1[{i},1] ({d1[i, 1]})"
-
-        # check
-        assert len(x.shape) == len(d1.shape) == len(d2.shape) == 2
-        assert x.shape[1] == d1.shape[0] == d2.shape[0]
-        assert d1.shape[1] == d2.shape[1] == 2
+        valid_lower = (x >= d1[0]) | np.isclose(x, d1[0])
+        valid_upper = (x <= d1[1]) | np.isclose(x, d1[1])
+        assert np.all(
+            valid_lower
+        ), f"Assertion failed: Some values are below lower bounds\n{x[~valid_lower]}"
+        assert np.all(
+            valid_upper
+        ), f"Assertion failed: Some values are above upper bounds\n{x[~valid_upper]}"
 
         # scale
-        for i in range(d):
-            x[:, i] = (x[:, i] - d1[i, 0]) / (d1[i, 1] - d1[i, 0])
-            x[:, i] = x[:, i] * (d2[i, 1] - d2[i, 0]) + d2[i, 0]
+        x = (x - d1[0]) / (d1[1] - d1[0])
+        x = x * (d2[1] - d2[0]) + d2[0]
 
         # Return in original shape
         return x.reshape(x_shape)
@@ -247,6 +236,68 @@ class Leja(GeneratorMultiD):
         else:
             raise
         self.domains = np.array(domains)
+        self._reference_domains = np.array([[-1, 1]] * len(domains))
 
     def __repr__(self) -> str:
         return f"Leja (d = {self.dim}, domains = {self.domains.tolist()})"
+
+    def scale_back(self, x: ArrayLike) -> ArrayLike:
+        return self.scale(x, d1=self.domains, d2=self._reference_domains)
+
+    def scale(
+        self, x: ArrayLike, d1: ArrayLike = None, d2: ArrayLike = None
+    ) -> ArrayLike:
+        """
+        Affine transformation from the interval d1 to the interval d2 applied to point x.
+        x : array or list of shape (n, d) or (d, )
+        d1, d2 : arrays or lists of shape (d, 2)
+        """
+        if d1 is None:
+            d1 = self._reference_domains
+        if d2 is None:
+            d2 = self.domains
+
+        # ensure d1, d2 have shape (d, 2)
+        d1, d2 = np.array(d1), np.array(d2)
+        assert d1.shape == d2.shape, f"shapes {d1.shape} and {d2.shape} do not match"
+        d = 1 if len(d1.shape) == 1 else d1.shape[0]
+        if len(d1.shape) == 1:
+            d1 = d1.reshape((1, 2))
+            d2 = d2.reshape((1, 2))
+        for i in range(d):
+            assert d1[i, 0] < d1[i, 1]
+            assert d2[i, 0] < d2[i, 1]
+
+        # ensure x has shape (n, d)
+        x = np.array(x)
+        x_shape = x.shape
+        if x_shape == ():
+            x = np.array([[x]])
+        else:
+            if len(x.shape) == 1:
+                if x.shape[0] == d:
+                    x = x.reshape((1, len(x)))
+                else:
+                    x = x.reshape((len(x), 1))
+
+        # ensure x in d1
+        valid_lower = (x >= d1[:, 0]) | np.isclose(x, d1[:, 0])
+        valid_upper = (x <= d1[:, 1]) | np.isclose(x, d1[:, 1])
+        assert np.all(
+            valid_lower
+        ), f"Assertion failed: Some values are below lower bounds\n{x[~valid_lower]}"
+        assert np.all(
+            valid_upper
+        ), f"Assertion failed: Some values are above upper bounds\n{x[~valid_upper]}"
+
+        # check
+        assert len(x.shape) == len(d1.shape) == len(d2.shape) == 2
+        assert x.shape[1] == d1.shape[0] == d2.shape[0]
+        assert d1.shape[1] == d2.shape[1] == 2
+
+        # scale
+        x = (x - d1[:, 0]) / (d1[:, 1] - d1[:, 0])
+        x = x * (d2[:, 1] - d2[:, 0]) + d2[:, 0]
+
+        # Return in original shape
+        return x.reshape(x_shape)
