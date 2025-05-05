3ce4af5ecf2ad43156c82a84f9341cf8afd968c6
Josephine Westermann <josephine.westermann@uni-heidelberg.de>
2025-05-05 14:50:27 +0200

    Made variables and names a bit more meaningful and added explanation

--- tests/test_smolyak_runtime.py
+++ tests/test_smolyak_runtime.py
@@ -4,41 +4,61 @@ import pytest
 from jax_smolyak import indices, nodes, smolyak
 
 
-def setup_nodes(d, nested, centered):
-    if nested:
-        if centered:
+def setup_nodes(d: int, node_type: str, default_domain: bool) -> nodes.Generator:
+    """
+    Set up a `nodes.Generator` instance based on the given type and domain configuration.
+
+    Parameters
+    ----------
+    d : int
+        The dimensionof the domain.
+    node_type : str
+        The type of nodes to set up. Must be one of {'leja', 'gauss'}.
+    default_domain : bool
+        If True, use the default domain for the specified node type.
+        If False, generate a random domain.
+
+    Returns
+    -------
+    nodes.Generator
+        An instance of the specified node type with the configured domain.
+    """
+    if node_type == 'leja':
+        if default_domain:
             return nodes.Leja(dim=d)
         else:
             domain = np.zeros((d, 2))
             domain[:, 1] = np.sort(np.random.rand(d) * 10)
             domain[:, 0] = -domain[:, 1]
             return nodes.Leja(domains=domain)
-    else:
-        if centered:
+    elif node_type == 'gauss':
+        if default_domain:
             return nodes.GaussHermite(dim=d)
         else:
             mean = np.random.randn(d)
             scaling = np.random.rand(d)
             return nodes.GaussHermite(mean, scaling)
+    else:
+        raise ValueError("node_type must be one of {'leja', 'gauss'}")
 
 
 @pytest.mark.parametrize(
-    "d, m, nested, centered",
+    "d, m, node_type, default_domain",
     [
-        (100, 1000, True, True),
-        (100, 1000, True, False),
-        (100, 1000, False, True),
-        (100, 1000, False, False),
-        (10000, 10000, True, True),
-        (10000, 10000, True, False),
-        (10000, 10000, False, True),
-        (10000, 10000, False, False),
+        (100, 1000, 'leja', True),
+        (100, 1000, 'leja', False),
+        (100, 1000, 'gauss', True),
+        (100, 1000, 'gauss', False),
+        (10000, 10000, 'leja', True),
+        (10000, 10000, 'leja', False),
+        (10000, 10000, 'gauss', True),
+        (10000, 10000, 'gauss', False),
     ],
 )
-def test_smolyak_constructor_runtime(benchmark, d, m, nested, centered):
-    node_gen = setup_nodes(d, nested, centered)
+def test_smolyak_constructor_runtime(benchmark, d, m, node_type, default_domain):
+    node_gen = setup_nodes(d, node_type, default_domain)
     k = np.log([2 + i for i in range(d)]) / np.log(2)
-    t = indices.find_approximate_threshold(k, m, nested)
+    t = indices.find_approximate_threshold(k, m, node_gen.is_nested)
     benchmark(lambda: smolyak.SmolyakBarycentricInterpolator(node_gen=node_gen, k=k, t=t, d_out=10))
 
 
@@ -47,43 +67,43 @@ def target_f(x, theta, r):
 
 
 @pytest.mark.parametrize(
-    "d, m, nested, centered",
+    "d, m, node_type, default_domain",
     [
-        (100, 1000, True, True),
-        (100, 1000, True, False),
-        (100, 1000, False, True),
-        (100, 1000, False, False),
-        (10000, 10000, True, True),
-        (10000, 10000, True, False),
-        (10000, 10000, False, True),
-        (10000, 10000, False, False),
+        (100, 1000, 'leja', True),
+        (100, 1000, 'leja', False),
+        (100, 1000, 'gauss', True),
+        (100, 1000, 'gauss', False),
+        (10000, 10000, 'leja', True),
+        (10000, 10000, 'leja', False),
+        (10000, 10000, 'gauss', True),
+        (10000, 10000, 'gauss', False),
     ],
 )
-def test_smolyak_set_f_runtime(benchmark, d, m, nested, centered):
-    node_gen = setup_nodes(d, nested, centered)
+def test_smolyak_set_f_runtime(benchmark, d, m, node_type, default_domain):
+    node_gen = setup_nodes(d, node_type, default_domain)
     k = np.log([2 + i for i in range(d)]) / np.log(2)
-    t = indices.find_approximate_threshold(k, m, nested)
+    t = indices.find_approximate_threshold(k, m, node_gen.is_nested)
     f = lambda x: target_f(x, 2.0, 2.0)
     benchmark(lambda: smolyak.SmolyakBarycentricInterpolator(node_gen=node_gen, k=k, t=t, d_out=1, f=f))
 
 
 @pytest.mark.parametrize(
-    "d, m, nested, centered",
+    "d, m, node_type, default_domain",
     [
-        (100, 1000, True, True),
-        (100, 1000, True, False),
-        (100, 1000, False, True),
-        (100, 1000, False, False),
-        (10000, 10000, True, True),
-        (10000, 10000, True, False),
-        (10000, 10000, False, True),
-        (10000, 10000, False, False),
+        (100, 1000, 'leja', True),
+        (100, 1000, 'leja', False),
+        (100, 1000, 'gauss', True),
+        (100, 1000, 'gauss', False),
+        (10000, 10000, 'leja', True),
+        (10000, 10000, 'leja', False),
+        (10000, 10000, 'gauss', True),
+        (10000, 10000, 'gauss', False),
     ],
 )
-def test_smolyak_eval(benchmark, d, m, nested, centered):
-    node_gen = setup_nodes(d, nested, centered)
+def test_smolyak_eval(benchmark, d, m, node_type, default_domain):
+    node_gen = setup_nodes(d, node_type, default_domain)
     k = np.log([2 + i for i in range(d)]) / np.log(2)
-    t = indices.find_approximate_threshold(k, m, nested)
+    t = indices.find_approximate_threshold(k, m, node_gen.is_nested)
     f = lambda x: target_f(x, 2.0, 2.0)
     smol = smolyak.SmolyakBarycentricInterpolator(node_gen=node_gen, k=k, t=t, d_out=1, f=f)
     x = np.random.randn(250, 1)
