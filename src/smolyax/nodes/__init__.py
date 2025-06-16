c5170c2000a826acafdd233ff2ab45a347dbd792
JoWestermann <91186482+JoWestermann@users.noreply.github.com>
2025-06-16 19:45:08 +0200

    Make interfaces and docstrings of nodes module clearer (#50)
    * Clearer separation between 1D node sequence generators and container class, along with improved documentation
    
    * Run tests with Leja nodes also on asymmetric domains

--- src/smolyax/nodes/__init__.py
+++ src/smolyax/nodes/__init__.py
@@ -1,5 +1,5 @@
-from .base import Generator
+from .base import Generator, Generator1D
 from .gausshermite import GaussHermite, GaussHermite1D
 from .leja import Leja, Leja1D
 
-__all__ = ["Generator", "Leja", "Leja1D", "GaussHermite1D", "GaussHermite"]
+__all__ = ["Generator1D", "Generator", "Leja1D", "Leja", "GaussHermite1D", "GaussHermite"]
